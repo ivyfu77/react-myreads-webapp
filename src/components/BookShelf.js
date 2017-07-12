ee274912479cc38e1a496dfbb5d8978b5b01f7f2
ivyfu77 <ivy@hmsnz.com>
2017-07-13 10:42:33 +1200

    Use BooksAPI to get the books info instead of hard coding it; seperate each book as a component in BookShelf

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,27 +1,14 @@
 import React from 'react';
+import Book from './Book';
 
 class BookShelf extends React.Component {
+  componentWillMount() {
+
+  }
   render() {
     let mappedBooks = this.props.books.map((book) => {
       return (
-        <li key={book.name}>
-          <div className="book">
-            <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("'+book.url+'")' }}></div>
-              <div className="book-shelf-changer">
-                <select>
-                  <option value="none" disabled>Move to...</option>
-                  <option value="currentlyReading">Currently Reading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
-              </div>
-            </div>
-            <div className="book-title">{book.name}</div>
-            <div className="book-authors">{book.author}</div>
-          </div>
-        </li>        
+        <Book key={book.id} book={book} />        
       );
     });
 
